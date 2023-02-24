484b2693fccc06f452eb49deaff8ae6626a1257b
Dat Ngo <dat_ngo10@protonmail.com>
2023-02-24 21:30:27 +0800

    Init: Initialise git and flutter project

---
+++ lib/pages/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called.
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: const Text("Home"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            const DrawerHeader(
+                decoration: BoxDecoration(color: Color(0xff764abc)),
+                child: Text("Draw Header")),
+            ListTile(
+              leading: const Icon(Icons
+                  .home), // Leading will display something before the title
+              title: const Text("Home"),
+              onTap: () {
+                Navigator.pop(
+                    context); // This will close the drawer after user taps on page
+
+                if (ModalRoute.of(context)?.settings.name == '/') return;
+
+                Navigator.pushNamed(context, '/');
+              },
+            )
+          ],
+        ),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Add Task',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
